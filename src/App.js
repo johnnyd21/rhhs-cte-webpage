9b1123333dcfda7ff76464ad3322ba165aff2ded
johnnyd21 <jdominguez@richmondhillhs.org>
2020-12-05 20:18:17 +0000

    changes2 getting state to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import './Bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
 import Academy_Card from './Components/Academy_Card';
 import LionsTV from './Components/LionsTV';
@@ -11,14 +12,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "number1": "Please select your Number 1 Choice of Academy:",
-      "number2": "Please select your Number 2 Choice of Academy:",
-      "number3": "Please select your Number 3 Choice of Academy:"
+      "choices":[]
     };
   }
-  makeChoice = (title, choice) => {
+  makeChoice = (choice) => {
     this.setState(state => {
-      state[title] = choice
+      state["choices"].push(choice)
       return state
     })
   }
@@ -34,24 +33,27 @@ class App extends React.Component {
           text="This pathway gives students the ability to be innovative with technology and learn a variety of computer programming languages." 
           img="https://inteng-storage.s3.amazonaws.com/img/iea/XD6KZvJ2Gv/sizes/software-trends_resize_md.jpg"
           alt="Image of Software Engineering Pathway"
-          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932698&type=d&pREC_ID=2065998" />
+          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932698&type=d&pREC_ID=2065998"
+          makeChoice={this.makeChoice} />
       
         <Academy_Card 
           title="Academy of Hospitality & Tourism" 
           text="Richmond Hill High Schools's Academy of Hospitality & Tourism helps students create career paths in one of the world’s and New York City's  largest industries, from hotels to sports, entertainment, and event management." 
           img="https://potomac.edu/wp-content/uploads/2019/04/Studying-Hospitality-Tourism.jpg"
           alt="Image of Academy of Hospitality & Tourism"
-          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932678&type=d&pREC_ID=2066003" />
+          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932678&type=d&pREC_ID=2066003"
+          makeChoice={this.makeChoice} />
 
         <Academy_Card 
           title="Art & Design Academy" 
           text="The Richmond Hill High School visual arts classes explore basic and advanced drawing, the world of ceramics, portfolio design and computer graphics." 
           img="https://i.guim.co.uk/img/media/e6ee88c4b60cd6fd315fb472beb8989920dd59a9/7_231_882_529/master/882.jpg?width=300&quality=85&auto=format&fit=max&s=b17e271238f5286e2fbfa3d4aca6f0b8"
           alt="Image of Art & Design"
-          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932672&type=d&pREC_ID=2066008"/>
+          webpage="https://www.richmondhillhs.org/apps/pages/index.jsp?uREC_ID=1932672&type=d&pREC_ID=2066008"
+          makeChoice={this.makeChoice}/>
         </div>
      
-        <Dashboard number1={this.state.number1} number2={this.state.number2} number3={this.state.number3} />
+        <Dashboard choices={this.state.choices} />
 
     </div>
           
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,9 +4,7 @@ function Dashboard(props) {
     <div className="Dashboard">
       <h3>Your Academy Dashboard</h3>
       <div className="choice-group">
-        <p className="choice">Number 1 Choice: {props.number1}</p>
-        <p className="choice">Number 2 Choice: {props.number2}</p>
-        <p className="choice">Number 3 Choice: {props.number3}</p>
+          {props.choices.map(choice => <p className="choice">Number 1 Choice: {choice}</p>)}
       </div>
     </div>
   );
